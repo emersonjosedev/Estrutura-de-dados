3c00f53cd3449a32d9694af4f29bf3be658948ba
alancodex <alansobraljs@gmail.com>
2025-06-09 17:58:40 -0300

    0.05

---
+++ aulas/Listas/listas/src/main/java/org/example/Lista.java
@@ -0,0 +1,38 @@
+package org.example;
+import java.util.ArrayList;
+ class Lista {
+    private String nome;
+    private  int idade;
+
+     public String getNome() {
+         return nome;
+     }
+
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+
+     public int getIdade() {
+         return idade;
+     }
+
+     public void setIdade(int idade) {
+         this.idade = idade;
+     }
+
+     public Lista(String nome, int idade){
+    this.nome = nome;
+    this.idade = idade;
+}
+
+
+
+
+
+    ArrayList<String> list1 = new ArrayList<String>();
+
+    public void inicarLista(){
+
+    }
+
+}
--- aulas/Listas/listas/src/main/java/org/example/Main.java
+++ aulas/Listas/listas/src/main/java/org/example/Main.java
@@ -1,8 +1,47 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    /*
+    Para definir o tipo do objeto ArrayList
+    Tipos Primitivos  =>    Wrapper Class
+    int  =>                 Integer
+    float  =>               Float
+    double =>               Double
+    char =>                 Character
+
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        Lista  l = new Lista();
+        ArrayList<String> p1 = l.list1;
+     p1.add("Emerson");
+     p1.add("Julia");
+
+        System.out.println(p1.get(0));
+
+     p1.remove(0);
+
+        //Ultimo da ArrayList
+        System.out.println(p1 +p1.get(p1.size() - 1));
+
+        //Tamanho da Array list
+        System.out.println(p1.size());
+
+        //foreatch
+        for(String nome : p1){
+            System.out.println(nome);
+        }
+
+        //Remove todos elementos da Array list
+        p1.clear();
+
+
+
     }
-    
+
+
 }
